66f1156d15d4f14aadbf40a2518c736de641084b
AntiDesert5 <axelvalenzuela7@gmail.com>
2019-03-27 22:50:45 -0600

    Initial commit

---
+++ app/src/main/java/com/example/redes/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.redes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button subneting;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button subneting = findViewById(R.id.subnetting);
+        
+    }
+}
